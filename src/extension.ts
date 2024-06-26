11b9ff64d605fed8a15c88ce1cefb83e9d4c1a3d
minionhub <minionhub.dev@gmail.com>
2024-06-25 17:04:16 -0700

    Add new feature suggestion for ddname

--- src/extension.ts
+++ src/extension.ts
@@ -40,6 +40,17 @@ async function loadCompany(url: any) {
     }
 }
 
+function getFieldsByDdname(companyList: any, ddname: any) {
+    for (const elem of companyList) {
+        for (const dd of elem.company.dd) {
+            if (dd.ddname == ddname) {
+                return dd.field;
+            }
+        }
+    }
+    return [];
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -51,6 +62,8 @@ export function activate(context: vscode.ExtensionContext) {
     if (!context.globalState.get("CompanyLibraries")) {
         context.globalState.update("CompanyLibraries", []);
     }
+
+    let companyList: any = context.globalState.get("CompanyLibraries");
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
@@ -80,8 +93,6 @@ export function activate(context: vscode.ExtensionContext) {
         panel.webview.onDidReceiveMessage(
             async message => {
                 // check the global state
-                let companyList: any = context.globalState.get("CompanyLibraries");
-
                 switch (message.command) {
                     case 'add_company':
                         if (companyList.findIndex((elem: any) => elem.company.cc === message.companyCode) >= 0) {
@@ -144,71 +155,72 @@ export function activate(context: vscode.ExtensionContext) {
         panel.webview.html = getHtmlForWebview(context.globalState.get("CompanyLibraries"));
     });
 
-    const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
+    // const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
 
-        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+    //     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
-            // a simple completion item which inserts `Hello World!`
-            const simpleCompletion = new vscode.CompletionItem('Hello World!');
+    //         // a simple completion item which inserts `Hello World!`
+    //         const simpleCompletion = new vscode.CompletionItem('Hello World!');
 
-            // a completion item that inserts its text as snippet,
-            // the `insertText`-property is a `SnippetString` which will be
-            // honored by the editor.
-            const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-            snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-            const docs: any = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
-            snippetCompletion.documentation = docs;
-            docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
+    //         // a completion item that inserts its text as snippet,
+    //         // the `insertText`-property is a `SnippetString` which will be
+    //         // honored by the editor.
+    //         const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+    //         snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+    //         const docs: any = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
+    //         snippetCompletion.documentation = docs;
+    //         docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
 
-            // a completion item that can be accepted by a commit character,
-            // the `commitCharacters`-property is set which means that the completion will
-            // be inserted and then the character will be typed.
-            const commitCharacterCompletion = new vscode.CompletionItem('console');
-            commitCharacterCompletion.commitCharacters = ['.'];
-            commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+    // a completion item that can be accepted by a commit character,
+    // the `commitCharacters`-property is set which means that the completion will
+    // be inserted and then the character will be typed.
+    // const commitCharacterCompletion = new vscode.CompletionItem('console');
+    // commitCharacterCompletion.commitCharacters = ['.'];
+    // commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
 
-            // a completion item that retriggers IntelliSense when being accepted,
-            // the `command`-property is set which the editor will execute after 
-            // completion has been inserted. Also, the `insertText` is set so that 
-            // a space is inserted after `new`
-            const commandCompletion = new vscode.CompletionItem('new');
-            commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-            commandCompletion.insertText = 'new ';
-            commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+    //         // a completion item that retriggers IntelliSense when being accepted,
+    //         // the `command`-property is set which the editor will execute after 
+    //         // completion has been inserted. Also, the `insertText` is set so that 
+    //         // a space is inserted after `new`
+    //         const commandCompletion = new vscode.CompletionItem('new');
+    //         commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+    //         commandCompletion.insertText = 'new ';
+    //         commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
 
-            // return all completion items as array
-            return [
-                simpleCompletion,
-                snippetCompletion,
-                commitCharacterCompletion,
-                commandCompletion
-            ];
-        }
-    });
+    //         // return all completion items as array
+    //         return [
+    //             simpleCompletion,
+    //             snippetCompletion,
+    //             commitCharacterCompletion,
+    //             commandCompletion
+    //         ];
+    //     }
+    // });
 
     const provider2 = vscode.languages.registerCompletionItemProvider(
         'plaintext',
         {
             provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+                const linePrefix = document.lineAt(position).text.substring(0, position.character);
+                const lastWordMatch = linePrefix.match(/(\w+)\.$/);
 
-                // get all text until the `position` and check if it reads `console.`
-                // and if so then complete if `log`, `warn`, and `error`
-                const linePrefix = document.lineAt(position).text.slice(0, position.character);
-                if (!linePrefix.endsWith('console.')) {
+                if (!lastWordMatch) {
                     return undefined;
                 }
 
-                return [
-                    new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-                    new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-                    new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-                ];
+                const ddname = lastWordMatch[1];
+                const fields = getFieldsByDdname(companyList, ddname);
+
+                if (!fields) {
+                    return undefined;
+                }
+                return fields.map((field: any) => new vscode.CompletionItem(field, vscode.CompletionItemKind.Field));
             }
         },
         '.' // triggered whenever a '.' is being typed
     );
 
-    context.subscriptions.push(disposable, provider1, provider2);
+    context.subscriptions.push(disposable, provider2);
 }
 
 function getHtmlForWebview(CompanyLibraries: any) {
